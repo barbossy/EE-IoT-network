c2eb54d63362d354dbe2f25a11c89ed3a98538ff
Bartosz Bossy <bartosz.bossy@wizzdev.pl>
2022-01-06 20:15:33 +0100

    Add unit tests

---
+++ EE-IoT-network/unit_test/include/end_user_mock.h
@@ -0,0 +1,4 @@
+#ifndef END_USER_MOCK_H
+#define END_USER_MOCK_H
+
+#endif // END_USER_MOCK_H
--- EE-IoT-network/unit_test/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "end_user.h"
-
-int main()
-{
-    std::cout << "Hello unit test" << std::endl;
-    return 0;
-}
